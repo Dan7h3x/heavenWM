2c666ca4b023fe41929060d55c12567567426f5b
Dan7h3x <m.jalili.barbin@gmail.com>
2023-04-29 10:06:21 +0330

    Suckless tools added.

---
+++ Configs/Suckless/DWM/layout.sh
@@ -0,0 +1,60 @@
+#!/usr/local/bin/bash
+# Layoutmenu script for dmenu
+
+choice=$(cat <<EOF | dmenu
+[\] Fibonacci
+|M| Centered
+><> Floating
+[]= Tiled
+||| Columns
+>M> CFM	
+[M] Monocle
+[D] Deck
+TTT Bstack
+=== BstrackHoriz
+::: Grid
+(@) Spiral
+|+| Tatami
+EOF)
+case $choice in
+'[\\] Fibonacci')
+	printf 0
+	;;
+'|M| Centered')
+	printf 1
+	;;
+'><> Floating')
+	printf 2
+	;;
+'[]= Tiled')
+	printf 3
+	;;
+'||| Columns')
+	printf 4
+	;;
+'>M> CFM')
+	printf 5
+	;;
+'[M] Monocle')
+	printf 6
+	;; 
+'[D] Deck')
+	printf 7
+	;;
+'TTT Bstack')
+	printf 8
+	;;
+'=== BstrackHoriz')
+	printf 9
+	;;
+'::: Grid')
+	printf 10
+	;;
+'(@) Spiral')
+	printf 11
+	;;
+'|+| Tatami')
+	printf 12
+	;;
+esac
+Footer
---
+++ Configs/Suckless/DWM/themes/tokyo.h
@@ -0,0 +1,43 @@
+static char c000000[]                    = "#03edf9"; // placeholder value
+
+static char normfgcolor[]                = "#ffffff";
+static char normbgcolor[]                = "#262335";
+static char normbordercolor[]            = "#ff7edb";
+static char normfloatcolor[]             = "#03edf9";
+
+static char selfgcolor[]                 = "#ffffff";
+static char selbgcolor[]                 = "#7aa2f7";
+static char selbordercolor[]             = "#ffa2f7";
+static char selfloatcolor[]              = "#ffaaaa";
+
+static char titlenormfgcolor[]           = "#A6ADc8";
+static char titlenormbgcolor[]           = "#15161E";
+static char titlenormbordercolor[]       = "#ff7edb";
+static char titlenormfloatcolor[]        = "#15161E";
+
+static char titleselfgcolor[]            = "#72f1b8";
+static char titleselbgcolor[]            = "#15161E";
+static char titleselbordercolor[]        = "#03edf9";
+static char titleselfloatcolor[]         = "#7aa2f7";
+
+static char tagsnormfgcolor[]            = "#ff7edb";
+static char tagsnormbgcolor[]            = "#15161E";
+static char tagsnormbordercolor[]        = "#f3e70f";
+static char tagsnormfloatcolor[]         = "#15161E";
+
+static char tagsselfgcolor[]             = "#7dcfff";
+static char tagsselbgcolor[]             = "#1a1b26";
+static char tagsselbordercolor[]         = "#7dcfff";
+static char tagsselfloatcolor[]          = "#7dcfff";
+
+static char hidnormfgcolor[]             = "#f7768e";
+static char hidselfgcolor[]              = "#7dcfff";
+static char hidnormbgcolor[]             = "#ffffff";
+static char hidselbgcolor[]              = "#15161E";
+
+static char urgfgcolor[]                 = "#c0caf5";
+static char urgbgcolor[]                 = "#15161E";
+static char urgbordercolor[]             = "#6bb2c0";
+static char urgfloatcolor[]              = "#15161E";
+
+

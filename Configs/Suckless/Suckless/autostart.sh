37463492dd95e5f77cc9dcd20206733ffb62d1f9
Dan7h3x <m.jalili.barbin@gmail.com>
2023-12-25 05:49:15 +0330

    --wip-- [skip ci]

---
+++ Configs/Suckless/Suckless/autostart.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#Notifications and statusbar
+dunst &
+mpd &
+#~/.local/bin/barinfo &
+killall dwmblocks
+dwmblocks &
+ksuperkey -e 'Super_L=Alt_L|F1' &
+#Nice Ones
+xsetroot -cursor_name left_ptr
+eww daemon &
+#Apps
+compfy &
+feh --bg-fill -z ~/.config/Wallpaper/Sonic.jpg &
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+setxkbmap us,ir -option 'grp:alt_shift_toggle' &
